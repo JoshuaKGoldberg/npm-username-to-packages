7a63bab2197009ce84bc9a89e6bc1b05bad123c4
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2024-11-27 19:39:06 -0500

    fix: missing & in registery query (#278)
    <!-- 👋 Hi, thanks for sending a PR to npm-username-to-packages! 💖.
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #277
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/npm-username-to-packages/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/npm-username-to-packages/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    The queried URL was passing a weird string for `from`.
    
    💖

--- src/npmUsernameToPackages.ts
+++ src/npmUsernameToPackages.ts
@@ -28,7 +28,7 @@ export async function npmUsernameToPackages(maintainer: string) {
 
 	while (true) {
 		const response = await fetch(
-			`https://registry.npmjs.com/-/v1/search?from=${packages.length}size=250&text=maintainer:${maintainer}`,
+			`https://registry.npmjs.com/-/v1/search?from=${packages.length}&size=250&text=maintainer:${maintainer}`,
 			{
 				headers: { "Content-Type": "application/json" },
 			},
